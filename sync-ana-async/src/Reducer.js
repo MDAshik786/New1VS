eb47d91f465fdef64d3b7cb80fea7929affa8b1a
MDAshik786 <rioashik786@gmail.com>
2023-09-14 21:41:07 +0530

    second

--- sync-ana-async/src/Reducer.js
+++ sync-ana-async/src/Reducer.js
@@ -1,17 +1,19 @@
 import React, { useReducer, useState } from 'react'
 const ACTION =  {
-   ADD_TODO: 'add-doto'
+   ADD_TODO: 'add-todo'
 }
-const reducer = (state,action) => {
+const reducer = (todo,action) => {
     console.log(action,"action")
- switch(action.tye){
+ switch(action.type){
     case ACTION.ADD_TODO:
-        return [...state, newTodo(action.payload.name)]
+        return [...todo, newTodo(action.payload.name)]
+   default:
+      return todo
  }
 
 }
 function newTodo(name){
-   return {id:Date.now, name, Complete:false}
+   return {id:Date.now(), name, Complete:false}
 }
 const Reducer = () => {
     const [todos , dispatch] = useReducer(reducer,[])
@@ -21,11 +23,22 @@ const handelClick = (e) => {
    e.preventDefault();
    dispatch({type:ACTION.ADD_TODO,payload:{name : Name} })
 }
+console.log(todos,"todo")
   return (
     <div>
         <form onSubmit={handelClick}>
           <input style={{height:"2rem",width:"10rem"}} type='text' value={Name} onChange={e => setName(e.target.value)}/>
         </form>
+         {todos.map((user) => {
+            return(
+               <div key={user.id} >
+           <span>Id : {user.id} </span>
+           <span>Name : {user.name} </span> 
+           <span>Complete : {user.Complete} </span>
+        </div>
+            )
+         })
+         }
     </div>
   )
 }
